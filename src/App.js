5a89b604ed398fcb21c322768640630a616705b0
AamenaK <aamenakausar8@gmail.com>
2022-03-03 14:00:15 +0530

    products in table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./footer";
-import Name from "./Name";
-import Counter from "./utils/Counter";
-
+import ProductList from "./ProductList";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
    //const h1 = React.createElement('h1', {}, 'Hello React' );
@@ -11,14 +10,9 @@ function App() {
    //const div = React.createElement('div', {}, [h1, img]);
    //return div;
 
-   return <div>
-       <Counter count = {10} />
-       <Counter count = {0} /> 
+   return <div> 
        <Header />
-       <h1>Hello React</h1>
-       <Name personName = "John" age = "21" />
-       <Name personName = "Joseph" age = "20" />
-       <img width = "200" height = "200" src = "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg" />
+       <ProductList />
        <Footer />
    </div>
 }

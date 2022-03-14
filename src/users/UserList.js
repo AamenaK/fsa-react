70317eb4a18b27b4f5de5fc47bd16270d598fe19
AamenaK <aamenakausar8@gmail.com>
2022-03-15 01:08:33 +0530

    Error handling

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -1,25 +1,24 @@
 import React from "react";
 import axios from 'axios';
 import User from './User';
-import LoaderImg from '../img/loader.gif';
 import ShouldRender from '../utils/ShouldRender';
 import Loader from '../utils/Loader';
 
 class UserList extends React.Component{
 
-    state = { users: [], loading:true };
+    state = { users: [], loading:true, error: false };
 
     constructor() {
         super();
         
         setTimeout(() => {
-            axios.get('https://api.github.com/users')
+            axios.get('https://api.github.com/users1')
                 .then( res => {
                     const data = res.data;
-                    this.setState({ loading: false, users : data });
+                    this.setState({ loading: false, users: data, error:false });
                 })
                 .catch( err => {
-                    this.setState({ loading: false });
+                    this.setState({ loading: false, error:true });
                     console.log(err); 
                 });
             }, 1000);
@@ -29,8 +28,13 @@ class UserList extends React.Component{
         return <div>
             <h1>Users</h1>
             <ShouldRender cond = {this.state.loading} >
-            <Loader />
-            </ShouldRender>        
+                <Loader />
+            </ShouldRender>   
+            <ShouldRender cond={this.state.error}>
+                <div className="col-md-8">
+                    <div class="alert alert-danger">Something went wrong. Try again</div>
+                </div>
+            </ShouldRender>    
             {this.state.users.map(user => <User key={user.login} user={user} /> )}
         </div>
     }

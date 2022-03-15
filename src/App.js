daa6145d8586e95f39eb7c3c29b95042f52d757f
AamenaK <aamenakausar8@gmail.com>
2022-03-15 13:00:30 +0530

    Single page app

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import './App.css';
 
 
 function App() {
-   return <div> 
-       <Header />
-       <AppRoutes />
-       <Footer />
-   </div>
+   return <AppRoutes />
 }
 
 
--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,11 +1,14 @@
+import Header from "./Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import UserList from "./users/UserList";
 import NotFound from "./NotFound";
+import Footer from "./footer";
 
 const AppRoutes = () => <BrowserRouter>
+    <Header />
     <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
@@ -15,6 +18,7 @@ const AppRoutes = () => <BrowserRouter>
         {/* <Route path="*" element={<Navigate to="/" />} /> */}
         <Route path="*" element={<NotFound />} />
     </Routes>
+    <Footer />
 </BrowserRouter>
 
 export default AppRoutes;

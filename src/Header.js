80d3081dafb3d33e9ae08e829622352ae6861a9a
AamenaK <aamenakausar8@gmail.com>
2022-03-30 20:46:36 +0530

    refactor

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import UserContext from './context/UserContext';
 import userService from './services/userService';
 import ShouldRender from './utils/ShouldRender';
 import { useNavigate } from 'react-router-dom';
-// import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+import IfElse from './utils/IfElse';
 
 function Header() {
     const { isLoggedIn, setLogin } = useContext(UserContext);
@@ -17,34 +17,34 @@ function Header() {
     }
 
     return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">FSA Jobs</a>
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/users">Users</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-        </ul>
-        <ShouldRender cond={!isLoggedIn}>
-            <div>
-                <Link to="/register" className="btn btn-sm btn-danger">Register</Link>=
-            </div>
-            <div>
-                <Link to="/login" className="btn btn-sm btn-danger">Login</Link>
-            </div>
-        </ShouldRender>
-        <ShouldRender cond={isLoggedIn}>
-        <button onClick={onLogout} className="btn btn-sm btn-danger">Logout</button>
-        </ShouldRender>
-    </div>
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">FSA Jobs</a>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/about">About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/users">Users</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                </li>
+            </ul>
+            <IfElse cond = {isLoggedIn}>
+                <button onClick={onLogout} className="btn btn-sm btn-danger">Logout</button>
+                <>
+                    <div>
+                        <Link to="/register" className="btn btn-sm btn-danger">Register</Link>
+                    </div>
+                    <div>
+                        <Link to="/login" className="btn btn-sm btn-danger">Login</Link>
+                    </div>
+                </>
+            </IfElse>
+        </div>
     </nav>
 }
 

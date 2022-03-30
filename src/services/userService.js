48635a1782e53d8e13e7656c4ce28a34d1325301
AamenaK <aamenakausar8@gmail.com>
2022-03-30 14:58:23 +0530

    File upload

--- src/services/userService.js
+++ src/services/userService.js
@@ -23,7 +23,11 @@ const saveUser = (user) => {
 }
 
 const update = (user) => {
-    return axios.put(`/api/users/${user.email}`, user, { headers: getHeaders() })
+    const headers = {
+        ...getHeaders(),
+        'content-type':'multipart/form-data'
+    }
+    return axios.put(`/api/users/${user.get('email')}`, user, { headers })
 };
 const getUser = (email) => {
     return axios.get(`/api/users/${email}`, { headers: getHeaders() });
